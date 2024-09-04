1a4a9287e3322b07f9eb40f6810f5afadeb6dcf5
Daniel <danielfernandes202@gmail.com>
2024-09-04 09:49:24 -0300

    save

--- src/glpi/task.py
+++ src/glpi/task.py
@@ -15,23 +15,25 @@ def task_glpi_create(self, datas: dict, files: List[dict]=None):
     glpi = GLPIFunctions(driver)
     glpi.login()
 
-    if retries:
-        if retries == 0:
+    try:
+        if retries:
+            if retries == 0:
+                archives = [Archive(file["filename"], file["content"]) for file in files] if files else []
+                response = glpi.open_request(protocolo, new_datas, archives)
+            else:
+                response = glpi.open_request(protocolo, new_datas)
+
+        else:
             archives = [Archive(file["filename"], file["content"]) for file in files] if files else []
             response = glpi.open_request(protocolo, new_datas, archives)
+
+        if response.sucess:
+            task_notification_wpp.delay('Ti Cisbaf', response.message)
+            task_notification_wpp.delay(new_datas.contact, f'O seu chamado foi aberto, caso você não tenha retorno, envie o protocolo #{protocolo} para a equipe de TI')
+            return "Chamado aberto com sucesso"
         else:
-            response = glpi.open_request(protocolo, new_datas)
-
-    else:
-        archives = [Archive(file["filename"], file["content"]) for file in files] if files else []
-        response = glpi.open_request(protocolo, new_datas, archives)
-
-    if response.sucess:
-        task_notification_wpp.delay('Ti Cisbaf', response.message)
-        task_notification_wpp.delay(new_datas.contact, f'O seu chamado foi aberto, caso você não tenha retorno, envie o protocolo #{protocolo} para a equipe de TI')
-        return "Chamado aberto com sucesso"
-    else:
-        task_notification_wpp.delay('TI Cisbaf', "Algum erro ao abrir o chamado no glpi, confira nas tasks do flower")
-        raise Exception(response.message)
-    
+            task_notification_wpp.delay('TI Cisbaf', "Algum erro ao abrir o chamado no glpi, confira nas tasks do flower")
+            raise Exception(response.message)
+    except Exception as exc:
+        self.retry(exc=exc)
 
